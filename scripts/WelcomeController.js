ba23311e4655cd8104a291a9efdb4a2136cad746
Marco Dumont <marco.dumont@gmail.com>
2016-05-04 10:31:25 +0000

    working login

--- scripts/WelcomeController.js
+++ scripts/WelcomeController.js
@@ -18,38 +18,70 @@
  * 
  */
 
+/*global angular*/
 var app = angular.module("ScheVaMEApp", ["firebase"]);
 
 app.controller("WelcomeController", function($scope, $firebaseAuth) {
+  /*global Firebase*/
   $scope.ref = new Firebase("https://blazing-fire-882.firebaseio.com/");
   // create an instance of the authentication service
   $scope.auth = $firebaseAuth($scope.ref);
-  // login with Facebook
-  
-  
-  $scope.doTwitterLogin = function () {
- 	$scope._login("twitter");
+
+$scope.user = {
+              provider: null,
+              name: "friend",
+              lastlogin: null
+            };
+
+  $scope.doTwitterLogin = function() {
+    $scope._login("twitter");
   };
-  
-  $scope.doFacebookLogin = function () {
-  	$scope._login("facebook");
+
+  $scope.doFacebookLogin = function() {
+    $scope._login("facebook");
   };
-  
-  $scope.doGoogleLogin = function () {
-  	$scope._login("google");
+
+  $scope.doGoogleLogin = function() {
+    $scope._login("google");
   };
-  
-  $scope.doGitHubLogin = function () {
-  	$scope._login("github");
+
+  $scope.doGitHubLogin = function() {
+    $scope._login("github");
   };
-  
-  $scope._login = function (provider) {
- 		$scope.auth.$authWithOAuthPopup(provider).then(function(authData) {
-  		console.log("Logged in as:", authData.uid);
-  		//replace the welcome message with projects
-  		
-  		}).catch(function(error) {
-    		alert("Authentication failed:" + error);
-  		});  	
+
+  $scope._login = function(provider) {
+      $scope.auth.$authWithOAuthPopup(provider).then(function(authData) {
+            console.log("Logged in as:", authData.uid);
+            $scope.user = {
+              provider: authData.provider,
+              name: $scope._getName(authData),
+              lastlogin: new Date().getTime()
+            };
+
+            if ($scope.ref.child("users").child(authData.uid)) {
+
+              $scope.ref.child("users").child(authData.uid).set($scope.user);
+            }
+        
+
+      //replace the welcome message with projects
+
+
+    }).catch(function(error) {
+      alert("Authentication failed:" + error);
+    });
+  };
+
+  $scope._getName = function(authData) {
+    switch (authData.provider) {
+      case 'google':
+        return authData.google.displayName;
+      case 'twitter':
+        return authData.twitter.displayName;
+      case 'facebook':
+        return authData.facebook.displayName;
+      case 'github':
+        return authData.github.displayName;
+    };
   };
 });
